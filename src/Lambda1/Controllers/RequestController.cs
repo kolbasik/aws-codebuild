beb8dcb505409ce494fce827a70e4fd6e48be25c
kolbasik <sergii.kolbasin@albelli.com>
2017-07-12 15:16:44 +0200

    added RequestController

---
+++ src/Lambda1/Controllers/RequestController.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lambda1.Controllers
+{
+    [Route("api/[controller]")]
+    public class RequestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Dictionary<string, object>
+            {
+                { nameof(Request.Method), Request.Method },
+                { nameof(Request.Scheme), Request.Scheme },
+                { nameof(Request.Host), Request.Host },
+                { nameof(Request.Path), Request.Path },
+                { nameof(Request.PathBase), Request.PathBase },
+                { nameof(Request.Query), Request.Query },
+                { nameof(Request.Headers), Request.Headers },
+            });
+        }
+    }
+}
